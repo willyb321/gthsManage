7ac968bc3949dd1c408d65114f24b6e56622c970
willyb321 <wbwilliam7@gmail.com>
2017-07-27 13:39:22 +1000

    Add short flag for config

--- cmd/root.go
+++ cmd/root.go
@@ -53,7 +53,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	home, _ := homedir.Dir()
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gthsManage.yaml)")
+	RootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.gthsManage.yaml)")
 	RootCmd.PersistentFlags().StringVarP(&ip, "ip", "i", "10.178.x.x", "IP of noticeboard")
 	RootCmd.PersistentFlags().StringVarP(&port, "port", "p", "1471", "SSH port of noticeboard")
 	RootCmd.PersistentFlags().StringVarP(&idRSA, "idfile", "f", home+"/.ssh/id_rsa", "Full path to your private ssh key")
