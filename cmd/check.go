4911dbe55c80c50f241cf249f3a46e22cc9ab113
willyb321 <wbwilliam7@gmail.com>
2017-07-27 17:30:06 +1000

    usage stuff

--- cmd/check.go
+++ cmd/check.go
@@ -27,13 +27,8 @@ import (
 // checkCmd represents the check command
 var checkCmd = &cobra.Command{
 	Use:   "check",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Check current config for errors",
+	Long:  `Check current config for errors`,
 	Run: func(cmd *cobra.Command, args []string) {
 		wasFine := checkConfig()
 		if wasFine != true {
--- cmd/create.go
+++ cmd/create.go
@@ -27,13 +27,8 @@ import (
 // createCmd represents the create command
 var createCmd = &cobra.Command{
 	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create config file based on questions",
+	Long:  `Create config file based on questions`,
 	Run: func(cmd *cobra.Command, args []string) {
 		createConfig()
 		fmt.Println("create called")
